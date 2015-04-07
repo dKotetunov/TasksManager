ad344ef438b056cc2be8d74c10fc706686ade61c
dKotetunov <dkotetunov@gmail.com>
2015-04-07 14:09:19 +0300

    Add tasks list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,27 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @project = Project.find(params[:project_id])
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @project = Project.find(params[:project])
+    @task = @project.task.create(params[:task])
+    redirect_to projects_tasks_path
+  end
+
+  def destroy
+    @project = Project.find(params[:project])
+    @task = @project.task.find(params[:id])
+    @task.destroy
+    redirect_to projects_tasks_path
   end
 end
