a420d9b40098f98dbb01fa3492fc223418d5f94f
dKotetunov <dkotetunov@gmail.com>
2015-04-07 17:15:43 +0300

    Add status for Task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_project
+
   def index
-    @tasks = Task.all
+    @tasks = Task.for_project(@project.id)
+    @tasks_done = @tasks.done
+    @task_started = @tasks.started
+    @task_not_started = @tasks.not_started
   end
 
   def new
@@ -8,20 +13,22 @@ class TasksController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:project_id])
-    @task = Task.find(params[:id])
+    @task = @project.tasks.find(params[:id])
   end
 
   def create
-    @project = Project.find(params[:project])
     @task = @project.task.create(params[:task])
     redirect_to projects_tasks_path
   end
 
   def destroy
-    @project = Project.find(params[:project])
     @task = @project.task.find(params[:id])
     @task.destroy
     redirect_to projects_tasks_path
   end
+
+  private
+   def set_project
+     @project = Project.find(params[:project_id])
+   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,14 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  attr_accessible :name, :description
+  attr_accessible :name, :description, :status, :project_id
+  DONE = 'done'
+  STARTED = 'started'
+  NOT_STARTED = 'not_started'
+
+  scope :for_project, ->(project_id) { where(:project_id => project_id)}
+  scope :done, -> {where(:status => 'done')}
+  scope :started, ->{where(:status =>'started')}
+  scope :not_started, ->{where(:status =>'not_started')}
+
 end
