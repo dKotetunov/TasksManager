d5f3a1170c1857a79218a2a90bfd626bbd91d7f8
dKotetunov <dkotetunov@gmail.com>
2015-04-14 15:29:16 +0300

    Little changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,6 @@
 class ProfilesController < ApplicationController
-<<<<<<< HEAD
-  before_action :set_profile, only :show
-=======
+
   before_action :set_profile, only: :show
->>>>>>> d94a95a0e9589c25e46e31431cb13e6cc0b10ee2
 
   def edit
     @profile = current_user.profile
