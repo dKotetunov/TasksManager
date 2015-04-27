0a3b571f1b780786a45e186a5157e23a130f3454
dKotetunov <dkotetunov@gmail.com>
2015-04-27 18:12:23 +0300

    Edit&Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,23 @@
 class CommentsController < ApplicationController
   before_action :set_task
-
+  before_action :set_comment, only: [:show,:destroy,:update, :edit]
   def create
-    @comment = @task.comments.create(params[:comment])
+    @comment = @task.comments.create(params[:comment].merge(user_id: current_user.id))
     redirect_to project_task_path(@comment.project, @comment.task)
   end
 
   def destroy
-    @comment = @comments.find(params[:id])
+    @comment = @task.comments.find(params[:id])
     @comment.destroy
-    redirect_to
+    redirect_to project_task_path(@comment.project, @comment.task)
+  end
+
+  def update
+    if @comment.update_attributes(params[:comment])
+      redirect_to project_task_path(@task.project, @task)
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -17,4 +25,9 @@ class CommentsController < ApplicationController
   def set_task
     @task = Task.find(params[:task_id])
   end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :task_id
+  attr_accessible :task_id, :text, :user_id
   belongs_to :user
   belongs_to :task
   delegate :project, to: :task
+
 end
