637b626f39de5ed746ed94320ab4df8889b41b31
Ruslan Milevskiy <rpmilevskiy@gmail.com>
2015-05-01 15:17:46 +0300

    Remove callback for default profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Role.find_or_create_by(name: "moderator")
 Role.find_or_create_by(name: "user")
 
 unless User.find_by_email('super@gmail.com')
-  admin = User.create(email: "super@gmail.com", password: "12345678", role: Role.admin)
+  admin = User.create(email: "super@gmail.com", password: "12345678", role: Role.admin, profile_attributes: { first_name: 'Super', last_name: 'Admin' })
   admin.skip_confirmation!
   admin.save
 end
