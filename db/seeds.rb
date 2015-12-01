922a63e58f6a4b6ada374a6059c4229431f12e15
dKotetunov <dkotetunov@gmail.com>
2015-12-01 10:48:35 +0200

    Add default user and moderator in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@ unless User.find_by_email('super@gmail.com')
   admin.skip_confirmation!
   admin.save
 end
+unless User.find_by_email('moderator@gmail.com')
+  admin = User.create(email: "moderator@gmail.com", password: "12345678", role: Role.moderator, profile_attributes: { first_name: 'First', last_name: 'Moderator' })
+  admin.skip_confirmation!
+  admin.save
+end
+unless User.find_by_email('user@gmail.com')
+  admin = User.create(email: "user@gmail.com", password: "12345678", role: Role.user, profile_attributes: { first_name: 'First', last_name: 'User' })
+  admin.skip_confirmation!
+  admin.save
+end
